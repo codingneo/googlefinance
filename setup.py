821890860735fbaff4d44c5019e7d1aebb44928e
Yiqun Hu <yiqun.hu@gmail.com>
2016-04-06 01:30:22 +0800

    Add zip_safe tag

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
   version = '0.7',
   description = 'Python module to get real-time (no delay) stock data from Google Finance API',
   author = 'Hongtao Cai',
+	zip_safe=True,
   author_email = 'caiht.ht@gmail.com',
   url = 'https://github.com/hongtaocai/googlefinance', # use the URL to the github repo
   download_url = 'https://github.com/hongtaocai/googlefinance/tarball/0.4', # I'll explain this in a second
